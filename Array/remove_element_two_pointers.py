2516df7faf1c8c99b53e10d7658329a5473bc4fc
Javier <javiero.barbosa@gmail.com>
2022-11-14 14:18:11 -0600

    Adding second approach

--- Array/remove_element_two_pointers.py
+++ Array/remove_element_two_pointers.py
@@ -31,6 +31,16 @@ class Solution:
         print(nums)
         return slow
 
+    def remove_duplicates_2(self, nums: List[int]) -> int:
+        # With no set
+        slow = 1
+        for fast in range(len(nums)-1):
+            if nums[fast] != nums[fast+1]:
+                nums[slow] = nums[fast+1]
+                slow += 1
+        print(nums)
+        return slow
+
 
 if __name__ == "__main__":
     so = Solution()
@@ -40,5 +50,5 @@ if __name__ == "__main__":
     k = 2
     # print(so.remove_element(arr_1, k))
     arr_2 = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
-    print(so.remove_duplicates(arr_2))
+    print(so.remove_duplicates_2(arr_2))
 
